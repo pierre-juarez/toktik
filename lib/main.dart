81a5eae2c20495a10c00e260863823893cea9db0
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-12-01 18:04:38 -0500

    [add] - Repository & datasources

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,11 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(videoPostDatasource: LocalVideoDatasource());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(videoRepository: videoPostRepository)..loadNextPage(),
         )
       ],
       child: MaterialApp(
